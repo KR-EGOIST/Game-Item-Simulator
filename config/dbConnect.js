d5f2bcbdb0056d464b1537bc7d2c7cb6d45feaa2
윤진호 <jhso5041@naver.com>
2024-05-14 15:55:03 +0900

    ✨ update : 몽고DB 재연결 기능 추가

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -20,8 +20,14 @@ const dbConnect = async () => {
   }
 };
 
-mongoose.connection.on('error', (err) => {
+// 몽구스 커넥션에 이벤트 리스너를 달게 해준다. 에러 발생 시 에러 내용을 기록하고, 연결 종료 시 재연결을 시도한다.
+https: mongoose.connection.on('error', (err) => {
   console.error('MongoDB 연결 에러', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.');
+  connect(); // 연결 재시도
 });
 
 export default dbConnect;
